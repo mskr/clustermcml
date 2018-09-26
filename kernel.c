0bb7b92d301349d8b24f1a5a95972e2431eaefda
Marius Kircher <marius.kircher@uni-ulm.de>
2018-09-26 10:05:06 +0200

    Changed kernel assert macro, experimental, needs tests

--- kernel.c
+++ kernel.c
@@ -93,23 +93,26 @@ double RandomNum(void) {
 #undef PI
 #define PI 3.14159265359f
 
-// An assert macro that writes error message to host buffer and returns from current function
+// An assert macro that writes error message to host buffer
+// and then performs a devide-by-zero to crash the kernel.
 //TODO dump the whole stack frame when assertions fail
 #ifdef DEBUG
-#define DEBUG_BUFFER_ARG ,__global char* debugBuffer
+#define DEBUG_BUFFER debugBuffer
+#define DEBUG_BUFFER_ARG ,__global char* DEBUG_BUFFER
 #define STR_COPY(src, dst) for(int i=0; src[i]!='\0';i++) dst[i]=src[i];
 #define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x) //The extra level of indirection will allow the preprocessor to expand the macros before they are converted to strings.
-#define classert(expr, t, a, b, c)\
+#define STR(x) STR_HELPER(x) // The extra level of indirection 
+// will allow the preprocessor to expand the macros 
+// before they are converted to strings.
+#define classert(expr)\
 	if(!(expr)) {\
-		const __constant char* msg = "error: assertion failed at line " STR(__LINE__);\
-		int i=0; for(; msg[i]!='\0';i++) debugBuffer[i]=msg[i];\
-		((__global t*)debugBuffer)[i] = a; ((__global t*)debugBuffer)[i+1] = b; ((__global t*)debugBuffer)[i+2] = c;\
-		return;\
+		const __constant char* _msg_ = "error: assertion failed at line " STR(__LINE__);\
+		STR_COPY(_msg_, DEBUG_BUFFER);\
+		DEBUG_BUFFER[0] = 42/0;\
 	}
-#else
+#else // no assert in non-debug mode
 #define DEBUG_BUFFER_ARG
-#define classert(expr, t, a, b, c)
+#define classert(expr)
 #endif
 
 // Following Nathan Reed's article:
@@ -440,7 +443,7 @@ DEBUG_BUFFER_ARG)
 		float s = -log(rand) / interactCoeff;
 		// output step lengths of as many threads as fit into debug buffer
 		// if (get_global_id(0) < 2048/4)
-		// 	((__global float*)debugBuffer)[get_global_id(0)] = s;
+		// 	((__global float*)DEBUG_BUFFER)[get_global_id(0)] = s;
 		// break;
 
 		// test ray-boundary-intersection
