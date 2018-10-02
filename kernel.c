385171fa04cad7b3927e2039725405c1bb00e878
Marius Kircher <marius.kircher@uni-ulm.de>
2018-10-02 15:08:14 +0200

    Separate classert.h

--- kernel.c
+++ kernel.c
@@ -5,7 +5,7 @@
 //#pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
 
 #ifdef CL2CPU
-#include "randomlib.h"
+#include "randomlib.h" // rand_lcg, rand_xorshift
 #else
 #define const __constant
 #define uint32_t uint
@@ -13,31 +13,10 @@
 #undef const
 #undef uint32_t
 #endif
+#include "classert.h" // DEBUG_BUFFER_ARG, classert
 
 #undef PI
 #define PI 3.14159265359f
-
-// An assert macro that writes error message to host buffer
-// and then performs a devide-by-zero to crash the kernel.
-//TODO dump the whole stack frame when assertions fail
-#ifdef DEBUG
-#define DEBUG_BUFFER debugBuffer
-#define DEBUG_BUFFER_ARG ,__global char* DEBUG_BUFFER
-#define STR_COPY(src, dst) for(int i=0; src[i]!='\0';i++) dst[i]=src[i];
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x) // The extra level of indirection 
-// will allow the preprocessor to expand the macros 
-// before they are converted to strings.
-#define classert(expr)\
-	if(!(expr)) {\
-		const __constant char* _msg_ = "error: assertion failed at line " STR(__LINE__);\
-		STR_COPY(_msg_, DEBUG_BUFFER);\
-		DEBUG_BUFFER[0] = 42/0;\
-	}
-#else // no assert in non-debug mode
-#define DEBUG_BUFFER_ARG
-#define classert(expr)
-#endif
 
 
 // Function to integrate by simpson kernel
