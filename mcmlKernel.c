6061eb0fa7ddd05529aa6d526ee3402aa5073a4f
Marius Kircher <marius.kircher@uni-ulm.de>
2018-10-02 15:16:35 +0200

    Separate kernel filesÂ

--- mcmlKernel.c
+++ mcmlKernel.c
@@ -18,50 +18,6 @@
 #undef PI
 #define PI 3.14159265359f
 
-
-// Function to integrate by simpson kernel
-float simpson_f(float x) {
-	return 4 / (1 + x*x);
-}
-
-// Simpson integration
-__kernel void simpson(float ha, float hb, __global float* out) {
-	size_t i = get_global_id(0);
-	size_t nthreads = get_global_size(0);
-	float a = ha + (hb - ha) * i/nthreads;
-	float b = ha + (hb - ha) * (i+1)/nthreads;
-	float value = simpson_f(a)/2 + simpson_f(b)/2;
-	float xleft;
-	float x = a;
-	int n = 1;
-	for (int i = 1; i < n; ++i) {
-		xleft = x;
-		x = a + i * (b - a) / n;
-		value += simpson_f(x) + 2 * simpson_f((xleft + x)/2);
-	}
-	value += 2 * simpson_f((x + b)/2);
-	value *= (b - a) / n / 3;
-	out[i] = value;
-}
-
-// Monte carlo approximation of PI
-__kernel void mcpi(const int npoints, __global uint* out) {
-	size_t i = get_global_id(0);
-	uint count = 0;
-	for (int j = 0; j < npoints; j++) {
-		uint rng_state = 0;
-		float x = (float)(rng_state = rand_xorshift(rng_state)) * RAND_NORM;
-		float y = (float)(rng_state = rand_xorshift(rng_state)) * RAND_NORM;
-		// check if inside quarter unit circle
-		if (x * x + y * y < 1.0f) {
-			count++;
-		}
-	}
-	out[i] = count;
-}
-
-/***** MCML *****/
-
 #include "Boundary.h"
 #include "Layer.h"
 #include "PhotonState.h"
---
+++ mcpiKernel.c
@@ -0,0 +1,21 @@
+#define const __constant
+#define uint32_t uint
+#include "randomlib.c"
+#undef const
+#undef uint32_t
+
+// Monte carlo approximation of PI
+__kernel void mcpi(const int npoints, __global uint* out) {
+	size_t i = get_global_id(0);
+	uint count = 0;
+	for (int j = 0; j < npoints; j++) {
+		uint rng_state = 0;
+		float x = (float)(rng_state = rand_xorshift(rng_state)) * RAND_NORM;
+		float y = (float)(rng_state = rand_xorshift(rng_state)) * RAND_NORM;
+		// check if inside quarter unit circle
+		if (x * x + y * y < 1.0f) {
+			count++;
+		}
+	}
+	out[i] = count;
+}
