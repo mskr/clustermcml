4925fd6e6db2a727ce5f92e116f19ea9cccf12c1
Marius Kircher <marius.kircher@uni-ulm.de>
2018-10-20 00:50:22 +0200

    Moved CL buffer handling from main to memory manager

--- main.cpp
+++ main.cpp
@@ -7,14 +7,12 @@
 #include "clusterlib.h"
 #endif
 
+#include "clmem.h"
+
 // interfaces for external code
-void allocCLKernelResources(size_t totalThreadCount, char* kernelOptions, char* otherOptions,
-	int* inputBufferCount, size_t* inputBufferSizes,
-	int* outputBufferCount, size_t* outputBufferSizes, int maxBufferCount, int rank);
-void runCLKernel(cl_context context, cl_command_queue cmdQueue, cl_kernel kernel, cl_mem* inputBuffers, cl_mem* outputBuffers,
-	size_t totalThreadCount, size_t simdThreadCount, int processCount, int rank);
+void allocCLKernelResources(size_t totalThreadCount, char* kernelOptions, char* otherOptions, int rank);
+void runCLKernel(cl_command_queue cmdQueue, cl_kernel kernel, size_t totalThreadCount, size_t simdThreadCount, int processCount, int rank);
 #ifdef GL_VISUALIZATION
-void createGLBuffer(size_t size, void* outBuffer);
 void runGLRenderLoop();
 #endif
 
@@ -30,11 +28,8 @@ int main(int nargs, char** args) {
 	char* filename = args[1];
 
 	// Bypass all multiprocessor stuff and use dummy info
-	int inputBufferCount = 0, outputBufferCount = 0;
-	size_t inputBufferSizes[10], outputBufferSizes[10];
-	int inputBuffers[10], outputBuffers[10];
-	allocCLKernelResources(1, "", filename, &inputBufferCount, inputBufferSizes, &outputBufferCount, outputBufferSizes, 10, 0);
-	runCLKernel(0, 0, 0, 0, 0, 1, 1, 1, 0);
+	allocCLKernelResources(1, "", filename, 0);
+	runCLKernel(0, 0, 1, 1, 1, 0);
 	return 0;
 
 	#else
@@ -53,56 +48,42 @@ int main(int nargs, char** args) {
 	// Get application specific command line options (input filenames etc.)
 	char* otherOptions = nargs >= 4 ? args[3] : "";
 
-	// Get application specific memory requirements
-	int inputBufferCount = 0, outputBufferCount = 0;
-	size_t inputBufferSizes[10], outputBufferSizes[10];
-	cl_mem inputBuffers[10], outputBuffers[10];
-	allocCLKernelResources(totalThreadCount, kernelOptions, otherOptions, &inputBufferCount, inputBufferSizes, &outputBufferCount, outputBufferSizes, 10, getRank());
+	setCLMemContext(context);
 
-	// Host buffers were created and sizes reported, now create CL buffers, optionally with GL interop
-	for (int i = 0; i < inputBufferCount; i++) {
-		inputBuffers[i] = CLCREATE(Buffer, context, CL_MEM_READ_ONLY, inputBufferSizes[i], NULL);
-	}
+	// Allocate resources based on application specific memory requirements
+	allocCLKernelResources(totalThreadCount, kernelOptions, otherOptions, getRank());
+
 	#ifdef GL_VISUALIZATION
-	bool* couldCreateGLBuffer = new bool[outputBufferCount]();
+	// Acquire shared GL buffers for use by CL from the device queue that does the visualization
+	for (int i = 0; i < getCLMemSharedGLObjectCount(); i++)
+		CL(EnqueueAcquireGLObjects, cmdQueue1, 1, &getCLMemSharedGLObject(i), 0, NULL, NULL);
 	#endif
-	for (int i = 0; i < outputBufferCount; i++) {
-		#ifdef GL_VISUALIZATION
-		unsigned int glBuf = 0;
-		createGLBuffer(outputBufferSizes[i], &glBuf);
-		if (glBuf) {
-			couldCreateGLBuffer[i] = true;
-			outputBuffers[i] = CLCREATE(FromGLBuffer, context, CL_MEM_READ_WRITE, glBuf);
-			CL(EnqueueAcquireGLObjects, cmdQueue1, 1, &outputBuffers[i], 0, NULL, NULL);
-		} else {
-			outputBuffers[i] = CLCREATE(Buffer, context, CL_MEM_READ_WRITE, outputBufferSizes[i], NULL);
-		}
-		#else
-		outputBuffers[i] = CLCREATE(Buffer, context, CL_MEM_READ_WRITE, outputBufferSizes[i], NULL);
-		#endif
-	}
 
 	// Run actual application
-	runCLKernel(context, cmdQueue1, kernel, inputBuffers, outputBuffers, totalThreadCount, simdThreadCount, processCount, getRank());
+	runCLKernel(cmdQueue1, kernel, totalThreadCount, simdThreadCount, processCount, getRank());
 
-	// Release objects so that they can be used by OpenGL
 	#ifdef GL_VISUALIZATION
-	for (int i = 0; i < outputBufferCount; i++) {
-		if (couldCreateGLBuffer[i]) {
-			CL(EnqueueReleaseGLObjects, cmdQueue1, 1, &outputBuffers[i], 0, NULL, NULL);
-		}
+	// Release shared GL buffers as they can now be used by GL
+	for (int i = 0; i < getCLMemSharedGLObjectCount(); i++)
+		CL(EnqueueReleaseGLObjects, cmdQueue1, 1, &getCLMemSharedGLObject(i), 0, NULL, NULL);
 	}
-	delete[] couldCreateGLBuffer;
 	runGLRenderLoop();
 	#endif
 
-	cleanupCluster(inputBufferCount, inputBuffers, outputBufferCount, outputBuffers, context, cmdQueue1, kernel);
+	freeCLMem();
+	cleanupCluster(context, cmdQueue1, kernel);
 
 	#endif // CL2CPU
 }
 
 // hello world example
 // http://phycomp.technion.ac.il/~comphy/classfiles/hellocl.c
+
+// Nvidia IHV Talk - Hardware and Optimizations (Timo Stich, Nvidia)
+// http://sa09.idav.ucdavis.edu/docs/SA09_NVIDIA_IHV_talk.pdf
+
+// AMD IHV Talk - Hardware and Optimizations (Jason Yang, AMD)
+// http://sa09.idav.ucdavis.edu/docs/SA09_AMD_IHV.pdf
 
 // nvidia optimization: occupancy, ...
 // https://www.cs.cmu.edu/afs/cs/academic/class/15668-s11/www/cuda-doc/OpenCL_Best_Practices_Guide.pdf
@@ -118,7 +99,12 @@ int main(int nargs, char** args) {
 // 2. compile kernel with C compiler (macro defines, no vector types) 
 // 3. Automatic scheduling of the submitted commands on OpenCL devices
 //    http://starpu.gforge.inria.fr/doc/html/SOCLOpenclExtensions.html
+// 4. Maybe more in: Advanced OpenCL Event Model Usage by Derek Gerstmann
+//    http://sa09.idav.ucdavis.edu/docs/SA09-opencl-dg-events-stream.pdf
 
 // Hot reloading hardcoded parameters
 // http://tuxedolabs.blogspot.com/2018/03/hot-reloading-hardcoded-parameters.html?m=1
 // File watcher: https://facebook.github.io/watchman/
+
+// Wave intrinsics from Microsoft shader compiler point of view
+// https://github.com/Microsoft/DirectXShaderCompiler/wiki/Wave-Intrinsics
