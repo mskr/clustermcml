c75192d72ac0b611145452504172371013a8c6c0
Marius Kircher <marius.kircher@uni-ulm.de>
2018-10-20 00:43:48 +0200

    Working memory manager that can take care of struct alignment, host and device memory allocation and different memory layouts

--- clmem.cpp
+++ clmem.cpp
@@ -1,14 +1,60 @@
+#include <assert.h>
 #include <map>
+#define DEBUG
+#include "clcheck.h" // CL macro
+void createGLBuffer(size_t size, void* outBuffer);
 
 typedef std::map<cl_mem, void*> Map;
-
 static Map map;
 
+// Current OpenCL context that keeps the device memory pool
+static cl_context context = 0;
+
+// Buffers that have been shared with OpenGL
+static cl_mem glObjects[10];
+static int glObjectCount = 0;
+int getCLMemSharedGLObjectCount() { return glObjectCount; }
+cl_mem getCLMemSharedGLObject(int i) { return glObjects[i]; }
+
+void setCLMemContext(cl_context c) {
+	context = c;
+}
 
 cl_mem allocCLInputBuffer(size_t size) {
-
+	assert(context);
+	cl_mem handle = CLCREATE(Buffer, context, CL_MEM_READ_ONLY, size, NULL);
+	void* pointer = malloc(size);
+	assert(pointer);
+	map[handle] = pointer;
+	return handle;
 }
 
 cl_mem allocCLOutputBuffer(size_t size) {
-	
+	assert(context);
+	cl_mem handle;
+	#ifdef GL_VISUALIZATION
+	unsigned int glBuffer = 0;
+	createGLBuffer(size, &glBuffer);
+	if (glBuffer) {
+		handle = CLCREATE(FromGLBuffer, context, CL_MEM_READ_WRITE, glBuffer);
+		glObjects[glObjectCount++] = handle;
+	} else handle = CLCREATE(Buffer, context, CL_MEM_READ_WRITE, size, NULL);
+	#else
+	handle = CLCREATE(Buffer, context, CL_MEM_READ_WRITE, size, NULL);
+	#endif
+	void* pointer = malloc(size);
+	assert(pointer);
+	map[handle] = pointer;
+	return handle;
+}
+
+void* getCLHostPointer(cl_mem handle) {
+	return map[handle];
+}
+
+void freeCLMem() {
+	for (const auto pair : map) {
+		CL(ReleaseMemObject, pair.first);
+		free(pair.second);
+	}
 }
--- clmem.h
+++ clmem.h
@@ -11,42 +11,60 @@
 #define ALIGN_NONE(T) __pragma(pack(push, 1)) T __pragma(pack(pop))
 #define ALIGN_4BYTE(T) __pragma(pack(push, 4)) T __pragma(pack(pop))
 #define ALIGN_8BYTE(T) __pragma(pack(push, 8)) T __pragma(pack(pop))
-#else // GCC or OpenCL
+#elif __GNUC__ || __OPENCL_VERSION__ // GCC or OpenCL
 #define ALIGN_NONE(T) T __attribute__((aligned(1)))
 #define ALIGN_4BYTE(T) T __attribute__((aligned(4)))
 #define ALIGN_8BYTE(T) T __attribute__((aligned(8)))
 #endif
 
-#define CLMALLOC(N, T) allocCLMem(N * sizeof(T));
 
 /**
 * Data Problem 2
 * --------------
 * Handles instead of pointers for host memory.
-* Safer and also usable as handles
-* for corresponding device memory.
+* Safer through centralized memory management.
+* Owner is always only this module.
+* Handles also valid for corresponding device memory.
 */
+#define CLMALLOC_INPUT(N, T) allocCLInputBuffer(N * sizeof(T));
+#define CLMALLOC_OUTPUT(N, T) allocCLOutputBuffer(N * sizeof(T));
 cl_mem allocCLInputBuffer(size_t size);
 cl_mem allocCLOutputBuffer(size_t size);
+void* getCLHostPointer(cl_mem handle);
 
-void* getCLMemHostPointer(cl_mem handle);
+/**
+* Set OpenCL context for which allocations will happen.
+* OpenCL shares memory for multiple devices via context.
+*/
+void setCLMemContext(cl_context);
+
+/**
+* Get output buffers that have been shared with OpenGL.
+* Sharing is done when compiled with GL_VISUALIZATION define.
+*/
+int getCLMemSharedGLObjectCount();
+cl_mem getCLMemSharedGLObject(int i);
+
+void freeCLMem();
 
 
 /**
 * Data Problem 3
 * --------------
-* Easily change memory layouts
-* via accessor functions.
+* Make memory layout explicit
+* and easy to change.
+* AOS == Array of Structs
+* SOA == Struct of Arrays
 */
 
-// For layout specific accessors we either
-// need a language feature to inspect structs or
-// write a method for each datatype or
-// use templates which we cannot compile for GPU.
-// The problem of switching data representations
-// by changing code only in one place
-// seems impossible to solve in C.
-
-enum Layout { ARRAY, ARRAY2D, AOS, SOA };
-
-void setCLMemAsAOS(void* pointer, size_t index, Layer data);
+#if __OPENCL_VERSION__
+#define CLMEM_ACCESS_ARRAY(pointer, i) pointer[i]
+#define CLMEM_ACCESS_ARRAY2D(pointer, size_j, i, j) pointer[i * size_j + j]
+#define CLMEM_ACCESS_AOS(pointer, i, member) pointer[i].member
+#define CLMEM_ACCESS_SOA(pointer, i, member) pointer->member[i]
+#else
+#define CLMEM_ACCESS_ARRAY(handle, T, i) ((T*)getCLHostPointer(handle))[i]
+#define CLMEM_ACCESS_ARRAY2D(handle, T, size_j, i, j) ((T*)getCLHostPointer(handle))[i * size_j + j]
+#define CLMEM_ACCESS_AOS(handle, T, i, member) ((T*)getCLHostPointer(handle))[i].member
+#define CLMEM_ACCESS_SOA(handle, T, i, member) ((T*)getCLHostPointer(handle))->member[i]
+#endif
