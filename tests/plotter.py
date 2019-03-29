72d5d1e08a389e1a25df020af7f7f4e188ec4fbc
Marius Kircher <marius.kircher@uni-ulm.de>
2019-03-29 16:40:43 +0100

    Test and plot scripts now support comparing two mcml implementations

--- tests/plotter.py
+++ tests/plotter.py
@@ -7,17 +7,36 @@ import os
 from enum import Enum
 import numpy as NUM
 import matplotlib.pyplot as PLT
+import argparse
 
-if len(sys.argv) < 2:
-	print('Usage: python plotter.py <list of mco files>')
-	exit()
+
+########################################################################################
+
+parser = argparse.ArgumentParser(description='Plot mco files')
+
+parser.add_argument('files', type=str, nargs='+',
+	help='list of mco files')
+
+parser.add_argument('--compare', dest='COMPARE_MODE_ENABLED', action='store_const', const=True, default=False,
+	help='Compare given mco files by using same plot for corresponding data')
+
+parser.add_argument('-o', dest='outFolder', action='store', default='comparison',
+	help='Output folder')
+
+args = parser.parse_args()
+
+
+########################################################################################
 
 Plottable = Enum('Plottable', 'NONE A_l A_z Rd_r Rd_a Tt_r Tt_a A_rz Rd_ra Tt_ra')
 
-# This function can plot and save data to a file...
-def plotToPDF(folder, components, data, resolutions, units):
+########################################################################################
 
-	PLT.figure()
+
+#
+def plotToPDF_internal(folder, components, data, resolutions, units):
+
+	path = ''
 
 	if len(components) == 2:
 		PLT.plot(data)
@@ -42,39 +61,55 @@ def plotToPDF(folder, components, data, resolutions, units):
 	else:
 		print('Cannot plot ' + str(len(components)) + '-dimensional data')
 
+	return path
+
+#
+def plotToPDF(folder, components, data, resolutions, units):
+
+	PLT.figure()
+
+	path = plotToPDF_internal(folder, components, data, resolutions, units)
+
 	if not os.path.exists(folder): os.makedirs(folder)
 	PLT.savefig(path)
 	PLT.close()
 	
 	print('Written ' + path)
 
+#
+def plotAllToPDF(folder, components, data_arrays, resolutions, units):
 
-# ... but first we need to extract the data from each mco file
-for i in range(1, len(sys.argv)):
+	PLT.figure()
+
+	path = ''
+
+	for data in data_arrays:
+		path = plotToPDF_internal(folder, components, data, resolutions, units)
+
+	if not os.path.exists(folder): os.makedirs(folder)
+
+	PLT.legend([i for i in range(0, len(data_arrays))]) #TODO pass executable names for the legend
+
+	PLT.savefig(path)
+	PLT.close()
+	
+	print('Written ' + path)
+
+
+########################################################################################
+
+
+#
+def parseInParams(stripped_lines):
 
 	n_layers = 0
 
 	dz = 0
 	dr = 0
+
 	nz = 0
 	nr = 0
 	na = 0
-
-	A_l = []
-	A_z = []
-	Rd_r = []
-	Rd_a = []
-	Tt_r = []
-	Tt_a = []
-	A_rz = [[]]
-	Rd_ra = [[]]
-	Tt_ra = [[]]
-
-	mco = sys.argv[i]
-	mco_file = open(mco)
-	lines = mco_file.read().splitlines()
-
-	stripped_lines = [l.strip() for l in lines if l.strip()!='' and l[0]!='#']
 
 	for i in range(0, len(stripped_lines)):
 		l = stripped_lines[i]
@@ -89,9 +124,28 @@ for i in range(1, len(sys.argv)):
 			n_layers = int(stripped_lines[i+5].split()[0])
 			break
 
+	# Check
 	if n_layers==0 or dz==0 or dr==0 or nz==0 or nr==0 or na==0:
 		print('layer and detection arrays must have non-zero size ('+mco+')')
 		exit()
+
+	return n_layers, dz, dr, nz, nr, na
+
+
+#
+def parseOutData(stripped_lines):
+
+	Rd_r = []
+	Rd_a = []
+	Rd_ra = [[]]
+
+	A_l = []
+	A_z = []
+	A_rz = [[]]
+
+	Tt_r = []
+	Tt_a = []
+	Tt_ra = [[]]
 
 	current_plottable = Plottable.NONE
 
@@ -159,37 +213,108 @@ for i in range(1, len(sys.argv)):
 				else:
 					Tt_ra.append([ float(numbers[i]) ])
 
-	if len(A_rz) != nr:
-		print('Incorrect data dimensions for A_rz ('+mco+')')
-		exit()
-	for l in A_rz:
-		if len(l) != nz:
-			print('Incorrect data dimensions for A_rz ('+mco+')')
-			exit()
+	return Rd_r, Rd_a, Rd_ra, A_l, A_z, A_rz, Tt_r, Tt_a, Tt_ra
 
+#
+def checkOutData(Rd_r, Rd_a, Rd_ra, A_l, A_z, A_rz, Tt_r, Tt_a, Tt_ra, nr, nz, na):
+
+	if len(Rd_r) != nr:
+		print('Incorrect data dimensions in Rd_r')
+		exit()
 	if len(Rd_ra) != nr:
-		print('Incorrect data dimensions for Rd_ra ('+mco+')')
+		print('Incorrect data dimensions in Rd_ra')
 		exit()
 	for l in Rd_ra:
 		if len(l) != na:
-			print('Incorrect data dimensions for Rd_ra ('+mco+')')
+			print('Incorrect data dimensions in Rd_ra')
+			exit()
+
+	if len(A_rz) != nr:
+		print('Incorrect data dimensions in A_rz')
+		exit()
+	for l in A_rz:
+		if len(l) != nz:
+			print('Incorrect data dimensions in A_rz')
 			exit()
 
 	if len(Tt_ra) != nr:
-		print('Incorrect data dimensions for Tt_ra ('+mco+')')
+		print('Incorrect data dimensions in Tt_ra')
 		exit()
 	for l in Tt_ra:
 		if len(l) != na:
-			print('Incorrect data dimensions for Tt_ra ('+mco+')')
+			print('Incorrect data dimensions in Tt_ra')
 			exit()
 
-	folder = mco[0:-4]
-	plotToPDF(folder, ['A', 'l'], A_l, [1], [''])
-	plotToPDF(folder, ['A', 'z'], A_z, [dz], ['cm'])
-	plotToPDF(folder, ['A', 'r', 'z'], A_rz, [dr, dz], ['cm', 'cm'])
-	plotToPDF(folder, ['Rd', 'r', 'a'], Rd_ra, [dr, na/90.0], ['cm', 'deg'])
-	plotToPDF(folder, ['Tt', 'r', 'a'], Tt_ra, [dr, na/90.0], ['cm', 'deg'])
-	plotToPDF(folder, ['Rd', 'r'], Rd_r, [dr], ['cm'])
-	plotToPDF(folder, ['Rd', 'a'], Rd_a, [na/90.0], ['deg'])
-	plotToPDF(folder, ['Tt', 'r'], Tt_r, [dr], ['cm'])
-	plotToPDF(folder, ['Tt', 'a'], Tt_a, [na/90.0], ['deg'])
+
+########################################################################################
+
+
+Rd_r_arrays = []
+Rd_a_arrays = []
+Tt_r_arrays = []
+Tt_a_arrays = []
+last_na = 0
+last_nr = 0
+last_dr = 0
+
+
+# Extract the data from each mco file
+for i in range(0, len(args.files)):
+
+	mco = args.files[i]
+
+	print('Parsing ' + mco + '...')
+
+	with open(mco) as f:
+
+		lines = f.read().splitlines()
+		stripped_lines = [l.strip() for l in lines if l.strip()!='' and l[0]!='#']
+
+
+		#TODO handle IGNORE_A case
+
+
+		n_layers, dz, dr, nz, nr, na = parseInParams(stripped_lines)
+
+		Rd_r, Rd_a, Rd_ra, A_l, A_z, A_rz, Tt_r, Tt_a, Tt_ra = parseOutData(stripped_lines)
+
+		checkOutData(Rd_r, Rd_a, Rd_ra, A_l, A_z, A_rz, Tt_r, Tt_a, Tt_ra, nr, nz, na)
+
+
+		if args.COMPARE_MODE_ENABLED:
+
+			# For now, only interested in 2-dimensional R and T data
+
+			Rd_r_arrays.append(Rd_r)
+			Rd_a_arrays.append(Rd_a)
+			Tt_r_arrays.append(Tt_r)
+			Tt_a_arrays.append(Tt_a)
+
+			if i > 0 and (na != last_na or nr != last_nr or dr != last_dr):
+				print('Cannot compare data with different scales')
+				exit()
+
+			last_na = na
+			last_nr = nr
+			last_dr = dr
+
+		else:
+
+			folder = mco[0:-4]
+			plotToPDF(folder, ['Rd', 'r'], Rd_r, [dr], ['cm'])
+			plotToPDF(folder, ['Rd', 'a'], Rd_a, [na/90.0], ['deg'])
+			plotToPDF(folder, ['Rd', 'r', 'a'], Rd_ra, [dr, na/90.0], ['cm', 'deg'])
+			plotToPDF(folder, ['A', 'l'], A_l, [1], [''])
+			plotToPDF(folder, ['A', 'z'], A_z, [dz], ['cm'])
+			plotToPDF(folder, ['A', 'r', 'z'], A_rz, [dr, dz], ['cm', 'cm'])
+			plotToPDF(folder, ['Tt', 'r', 'a'], Tt_ra, [dr, na/90.0], ['cm', 'deg'])
+			plotToPDF(folder, ['Tt', 'r'], Tt_r, [dr], ['cm'])
+			plotToPDF(folder, ['Tt', 'a'], Tt_a, [na/90.0], ['deg'])
+
+
+if args.COMPARE_MODE_ENABLED:
+
+	plotAllToPDF(args.outFolder, ['Rd', 'r'], Rd_r_arrays, [last_dr], ['cm'])
+	plotAllToPDF(args.outFolder, ['Rd', 'a'], Rd_a_arrays, [na/90.0], ['deg'])
+	plotAllToPDF(args.outFolder, ['Tt', 'r'], Tt_r_arrays, [dr], ['cm'])
+	plotAllToPDF(args.outFolder, ['Tt', 'a'], Tt_a_arrays, [na/90.0], ['deg'])
--- tests/tester.py
+++ tests/tester.py
@@ -8,6 +8,7 @@ import sys
 import os
 import subprocess
 import tempfile
+import argparse
 
 
 
@@ -36,7 +37,7 @@ mus = [1, 10, 100] # scattering events per unit
 g = [0, 0.5, 0.9]  # scattering anisotropy https://omlc.org/classroom/ece532/class3/hg.html
 n = [1.34, 1.4, 1.44] # refractive indices of Caucasian skin (from table 2 in )
 
-PHOTON_MILLIONS = 10
+PHOTON_MILLIONS = 1
 
 boundary_presets = { # user can either choose one from here or use implicit flat boundaries
 	'flat50': {
@@ -73,7 +74,7 @@ mpiCmd = ['mpiexec', '-debug', str(mpiDebugLevel), '-lines', '-machinefile', mpi
 # Interpret options:
 
 if len(sys.argv) < 2:
-	print('Usage: python tester.py <mcml executable> <optional executable args> -b <optional list of explicit boundaries(*)> -o <optional output folder>')
+	print('Usage: python tester.py <mcml executable> <optional executable args> --b <first explicit boundaries(*)> -o <optional output folder> -c <other mcml executable to compare with>')
 	
 	print('\nThis script will run multiple MCML simulations with a user-specified implementation, producing mco files.')
 	print('Plots will be created at the end from the mco files, for which it depends on another script plotter.py.')
@@ -97,14 +98,24 @@ if len(sys.argv) < 2:
 	exit()
 
 
+
+#TODO proper argument handling
+
+# parser = argparse.ArgumentParser('Test mcml implementation with many input parameter combinations')
+
+# parser.add_argument('mcml_exe', type=str, nargs='+', help='mcml executable')
+
+# parser.add_argument('-b', dest='') #TODO new concept for providing list of explicit boundaries
+
+
 mcml_exe = []
-while len(sys.argv)>1 and sys.argv[1]!='-b' and sys.argv[1]!='-o':
+while len(sys.argv)>1 and sys.argv[1]!='-b' and sys.argv[1]!='-o' and sys.argv[1]!='-c':
 	mcml_exe.append(sys.argv.pop(1))
 
 explicit_boundaries = []
 if len(sys.argv)>2 and sys.argv[1]=='-b':
 	sys.argv.pop(1)
-	while len(sys.argv)>1 and sys.argv[1]!='-o':
+	while len(sys.argv)>1 and sys.argv[1]!='-o' and sys.argv[1]!='-c':
 		b = sys.argv.pop(1)
 		try:
 			explicit_boundaries.append(boundary_presets[b]['data'])
@@ -113,12 +124,55 @@ if len(sys.argv)>2 and sys.argv[1]=='-b':
 			exit()
 
 out_folder = os.getcwd() + '/'
-if len(sys.argv)>2 and sys.argv.pop(1)=='-o':
+if len(sys.argv)>2 and sys.argv[1]=='-o':
+	sys.argv.pop(1)
 	out_folder += sys.argv.pop(1) + '/'
 if not os.path.exists(out_folder): os.makedirs(out_folder)
 
+compare_mcml_exe = []
+if len(sys.argv)>2 and sys.argv[1]=='-c':
+	sys.argv.pop(1)
+	compare_mcml_exe.append(sys.argv.pop(1))
+
+COMPARE_MODE_ENABLED = (len(compare_mcml_exe) > 0)
 
 
+
+
+
+
+def makeMCIFile(mci, mco, mua_i, mus_i, g_i, n_i, use_explicit_boundaries):
+	with open(mci, 'w') as f:
+		f.write('1.0\n') # file version
+		f.write('1\n') # number of sims
+
+		# sim 1
+		f.write(mco+' A\n') # output filename, ASCII
+		f.write(str(PHOTON_MILLIONS*1000000) + '\n') # number of photons
+		f.write(str(dz)+' '+str(dr)+'\n') # detection deltas in cm (dz, dr)
+		f.write(str(nz)+' '+str(nr)+' '+str(na)+'\n') # detection array dims (z, r, a)
+
+		# number of layers
+		if use_explicit_boundaries and len(explicit_boundaries) > 1:
+			f.write(str(len(explicit_boundaries)-1) + '\n')
+		else:
+			f.write('1\n')
+
+		f.write('1.0\n') # n above
+
+		if use_explicit_boundaries and len(explicit_boundaries) > 1:
+			for i in range(0, len(explicit_boundaries)-1):
+				f.write(' '.join(str(f) for f in explicit_boundaries[i]) + '\n')
+				f.write(str(n_i)+' '+str(mua_i)+' '+str(mus_i)+' '+str(g_i)+' '+str(dl)+'\n')
+			f.write(' '.join(str(f) for f in explicit_boundaries[len(explicit_boundaries)-1]) + '\n')
+		elif use_explicit_boundaries and len(explicit_boundaries) > 0:
+			f.write(' '.join(str(f) for f in explicit_boundaries[0]) + '\n')
+			f.write(str(n_i)+' '+str(mua_i)+' '+str(mus_i)+' '+str(g_i)+' '+str(dl)+'\n')
+			f.write(' '.join(str(f) for f in explicit_boundaries[0]) + '\n')
+		else:
+			f.write(str(n_i)+' '+str(mua_i)+' '+str(mus_i)+' '+str(g_i)+' '+str(dl)+'\n') # layer 1
+
+		f.write('1.0') # n below
 
 
 
@@ -127,91 +181,87 @@ if not os.path.exists(out_folder): os.makedirs(out_folder)
 
 mcos = []
 
+simcount = 0
+maxsims = str(len(mua)*len(mus)*len(g)*len(n))
+
 for mua_i in mua:
 	for mus_i in mus:
 		for g_i in g:
 			for n_i in n:
 
-				mco = os.path.abspath(out_folder + str(PHOTON_MILLIONS)+'M_'+str(mua_i)+'mua_'+str(mus_i)+'mus_'+str(g_i)+'g_'+str(n_i)+'n.mco')
+				simcount += 1
 
-				if (os.path.isfile(mco)):
-					print('Skip existing ' + str(mco))
-					mcos.append(mco)
-					continue
+				C = (2 if COMPARE_MODE_ENABLED else 1)
+				is_compare = 1
 
-				mci = os.path.join(tempfile.mkdtemp(), 'test.mci')
-				with open(mci, 'w') as f:
-					f.write('1.0\n') # file version
-					f.write('1\n') # number of sims
+				for c_i in range(0, C):
 
-					# sim 1
-					f.write(mco+' A\n') # output filename, ASCII
-					f.write(str(PHOTON_MILLIONS*1000000) + '\n') # number of photons
-					f.write(str(dz)+' '+str(dr)+'\n') # detection deltas in cm (dz, dr)
-					f.write(str(nz)+' '+str(nr)+' '+str(na)+'\n') # detection array dims (z, r, a)
+					mco = out_folder + str(PHOTON_MILLIONS)+'M_'+str(mua_i)+'mua_'+str(mus_i)+'mus_'+str(g_i)+'g_'+str(n_i)+'n'
 
-					# number of layers
-					if len(explicit_boundaries) > 1:
-						f.write(str(len(explicit_boundaries)-1) + '\n')
-					else:
-						f.write('1\n')
+					if c_i==is_compare:
+						mco += '_compare'
 
-					f.write('1.0\n') # n above
+					mco = os.path.abspath(mco + '.mco')
 
-					if len(explicit_boundaries) > 1:
-						for i in range(0, len(explicit_boundaries)-1):
-							f.write(' '.join(str(f) for f in explicit_boundaries[i]) + '\n')
-							f.write(str(n_i)+' '+str(mua_i)+' '+str(mus_i)+' '+str(g_i)+' '+str(dl)+'\n')
-						f.write(' '.join(str(f) for f in explicit_boundaries[len(explicit_boundaries)-1]) + '\n')
-					elif len(explicit_boundaries) > 0:
-						f.write(' '.join(str(f) for f in explicit_boundaries[0]) + '\n')
-						f.write(str(n_i)+' '+str(mua_i)+' '+str(mus_i)+' '+str(g_i)+' '+str(dl)+'\n')
-						f.write(' '.join(str(f) for f in explicit_boundaries[0]) + '\n')
-					else:
-						f.write(str(n_i)+' '+str(mua_i)+' '+str(mus_i)+' '+str(g_i)+' '+str(dl)+'\n') # layer 1
+					if (os.path.isfile(mco)):
+						print('[SKIP] existing ' + str(mco))
+						continue
 
-					f.write('1.0') # n below
+					mci = os.path.join(tempfile.mkdtemp(), 'test.mci')
 
-
-				print('===========================================================')
-				print('[STARTING] Simulation ' + str(len(mcos)+1) + ' of ' + str(len(mua)*len(mus)*len(g)*len(n)))
-				print('===========================================================')
-				print('[INPUT] ' + mci + ':')
-				with open(mci, 'r') as f:
-					print(f.read())
-				print('===========================================================')
-
-				success = False
-
-				while not success:
-
-					pathPair = os.path.split(mcml_exe[0]) #(head, tail) where tail is the last pathname component and head is everything leading up to that
-
-					cmd = [*mcml_exe, mci]
-
-					if useMPI:
-						cmd[0] = pathPair[1]
-						cmd = mpiCmd + cmd
-
-					print('[EXEC] ' + str(cmd) + ':')
-					result = subprocess.run(cmd, cwd=pathPair[0])
+					makeMCIFile(mci, mco, mua_i, mus_i, g_i, n_i, (len(explicit_boundaries) > 0 and c_i!=is_compare))
 
 					print('===========================================================')
-					try:
-						result.check_returncode()
-						print('[OUTPUT] ' + mco)
+					print('[STARTING] Simulation ' + str(simcount) + ' of ' + str(maxsims))
+					print('===========================================================')
+					print('[INPUT] ' + mci + ':')
+					with open(mci, 'r') as f:
+						print(f.read())
+					print('===========================================================')
 
-						print('[PLOTTING]')
-						subprocess.run(['python', 'plotter.py', mco], stdin=subprocess.DEVNULL, universal_newlines=True)
+					success = False
 
-						mcos.append(mco)
+					while not success:
 
-						success = True
+						pathPair = os.path.split(mcml_exe[0]) #(head, tail) where tail is the last pathname component and head is everything leading up to that
 
-					except subprocess.CalledProcessError as err:
-						print('[FAILED] ' + str(err))
-						print('Retrying...')
-					print('\n\n')
+						cwd = pathPair[0]
+
+						cmd = [*mcml_exe, mci]
+
+						if useMPI:
+							cmd[0] = pathPair[1]
+							cmd = mpiCmd + cmd
+
+						if c_i==is_compare:
+							cmd = [*compare_mcml_exe, mci]
+							cwd = '.'
+
+						print('[EXEC] ' + str(cmd) + ':')
+						result = subprocess.run(cmd, cwd=cwd)
+
+						print('===========================================================')
+						try:
+							result.check_returncode()
+
+							print('[OUTPUT] ' + mco)
+
+
+							if COMPARE_MODE_ENABLED and c_i==is_compare:
+								print('[PLOTTING]')
+								subprocess.run(['python', 'plotter.py', mcos[len(mcos)-1], mco, '--compare', '-o', mco[0:-4]])
+							elif not COMPARE_MODE_ENABLED:
+								print('[PLOTTING]')
+								subprocess.run(['python', 'plotter.py', mco])
+
+							mcos.append(mco)
+
+							success = True
+
+						except subprocess.CalledProcessError as err:
+							print('[FAILED] ' + str(err))
+							print('Retrying...')
+						print('\n\n')
 
 
 print('[FINISHED] ' + str(len(mcos)) + ' mco files produced')
