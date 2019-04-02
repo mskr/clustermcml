2b1d153775e12cba11c5c7b4e588eda93163f8f4
Marius Kircher <marius.kircher@uni-ulm.de>
2019-04-02 16:09:10 +0200

    Early out in mcml kernel only for photons that are actually marked as dead

--- mcmlKernel.c
+++ mcmlKernel.c
@@ -348,9 +348,7 @@ DEBUG_BUFFER_ARG) // optional debug buffer
 	// Get current photon state
 	__global struct PhotonTracker* state = &photonStates[get_global_id(0)];
 
-	// Photon got marked as dead because we are in process of finishing or
-	// photon weight is still zero because it was not restarted to save MPI communication
-	if (state->isDead || state->weight == 0) {
+	if (state->isDead) {
 		// This photon was not restarted because enough are in the pipeline
 		return; // I have no work :(
 	}
